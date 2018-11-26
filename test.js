46f720572801158603c4a78feaba30195c6b5aed
LeksVK <35683718+LeksVK@users.noreply.github.com>
2018-11-26 22:33:34 +0400

    Change header
    Restyle header

--- test.js
+++ test.js
@@ -1,24 +1,76 @@
             /** LogIn Form**/
 
-var logForm = document.querySelector('.login_form');
-var cover = document.querySelector('.back_login');
+var logForm = document.querySelector('.log-form');
+var cover   = document.querySelector('.log-form__back');
 
 function login() {
-    logForm.classList.add('login_form_active');
-    cover.classList.remove('back_login_off');
-    cover.classList.add('back_login_active');     
+    logForm.classList.add('j-log-form_active');
+    cover.classList.remove('j-log-form__back_off');
+    cover.classList.add('j-log-form__back_active');
 };
 
 function logout() {
-    logForm.classList.remove('login_form_active');
-    cover.classList.add('back_login_off');
-    cover.classList.remove('back_login_active');
+    logForm.classList.remove('j-log-form_active');
+    cover.classList.add('j-log-form__back_off');
+    cover.classList.remove('j-log-form__back_active');
 };
 
-cover.addEventListener('click', function () {
-    logForm.classList.remove('login_form_active');
-    cover.classList.remove('back_login_active');
-    cover.classList.add('back_login_off');
+cover.addEventListener('click', function() {
+    logForm.classList.remove('j-log-form_active');
+    cover.classList.remove('j-log-form__back_active');
+    cover.classList.add('j-log-form__back_off');
+});
+
+    /*** Slider IN banner ***/
+var wallpaper = document.querySelector('.promo');
+var nav_left  = document.querySelector('.promo__scroll_left');
+var nav_right = document.querySelector('.promo__scroll_right');
+    var i     = 0;
+    var j     = 0;
+
+nav_left.addEventListener('click', function() {
+    console.log(i);
+    if (i == 0) {
+        wallpaper.classList.remove('j-promo_back-bridge');
+        wallpaper.classList.add('j-promo_back-lake');
+            i = 1;
+         console.log(i);
+
+    } else if (i == 1) {
+        wallpaper.classList.remove('j-promo_back-lake');
+        wallpaper.classList.add('j-promo_back-night');
+            i = 2;
+         console.log(i);
+
+    } else if (i == 2) {
+        wallpaper.classList.remove('j-promo_back-night');
+        wallpaper.classList.add('j-promo_back-bridge');
+            i = 0;
+         console.log(i);
+    }
+});
+
+nav_right.addEventListener('click', function() {
+      var yes = 'yes';
+    console.log(yes);
+    if (i == 0) {
+        wallpaper.classList.remove('j-promo_back-bridge');
+        wallpaper.classList.add('j-promo_back-night');
+            i = 1;
+         console.log(i);
+
+    } else if (i == 1) {
+        wallpaper.classList.remove('j-promo_back-night');
+        wallpaper.classList.add('j-promo_back-lake');
+            i = 2;
+         console.log(i);
+
+    } else if (i == 2) {
+        wallpaper.classList.remove('j-promo_back-lake');
+        wallpaper.classList.add('j-promo_back-bridge');
+            i = 0;
+         console.log(i);
+    }
 });
 
    /** Sub-menu IN Special offers **/
@@ -82,54 +134,3 @@ function dellmenu6() {
 var list = document.querySelector('.sub6_list');
 list.classList.add('hidden6_item');
 };
-        /*** Slider IN banner ***/
-
-var wallpaper = document.querySelector('.banner');
-var nav_left = document.querySelector('.scroll_left');
-var nav_right = document.querySelector('.scroll_right');
-var i = 0;
-var j = 0;
-
-nav_left.addEventListener('click', function () {
-    console.log(i);
-    if (i == 0) {
-        wallpaper.classList.remove('banner_back_promo');
-        wallpaper.classList.add('banner_back_lake');
-        i = 1;
-         console.log(i);
-    } else if (i == 1) {
-        wallpaper.classList.remove('banner_back_lake');
-        wallpaper.classList.add('banner_back_night');
-        i = 2;
-         console.log(i);
-    } else if (i == 2) {
-        wallpaper.classList.remove('banner_back_night');
-        wallpaper.classList.add('banner_back_promo');
-        i = 0;
-         console.log(i);
-    }
-});
-
-nav_right.addEventListener('click', function (evt) {
-    var yes = 'yes';
-    console.log(yes);
-/*    j++;
-    console.log(j); */
-    if (i == 0) {
-        wallpaper.classList.remove('banner_back_promo');
-        wallpaper.classList.add('banner_back_night');
-        i = 1;
-         console.log(i);
-    } else if (i == 1) {
-        wallpaper.classList.remove('banner_back_night');
-        wallpaper.classList.add('banner_back_lake');
-        i = 2;
-         console.log(i);
-    } else if (i == 2) {
-        wallpaper.classList.remove('banner_back_lake');
-        wallpaper.classList.add('banner_back_promo');
-        i = 0;
-         console.log(i);
-    }
-
-});
